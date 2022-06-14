366717e787917a1731ffa532948bb78352a74284
hefeixiong <1074535830@qq.com>
2022-06-14 16:49:17 +0800

    测试user接口

--- app/router.js
+++ app/router.js
@@ -6,6 +6,6 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  router.get('/user', controller.user.info);
+  router.resources('user', '/user', controller.user);
   router.get('/news', controller.news.list);
 };
